15871dc6fd78f6ac2657930f2a53dcbe99c9c32e
Chandan Aulakh <49302381+chandan1600@users.noreply.github.com>
2021-02-09 12:46:21 +1300

    CryptoCurrencyTracker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+ import axios from 'axios';
+ import React, {useState, useEffect} from 'react';
+ import Coin from './Coin';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=NZD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data);
+      })
+      .catch(error => console.log(error));
+  });  
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoin = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="crypto-coin">
+      <div className="coin-search">
+        <h1 className="coinText">Search a Currency</h1>
+        <form>
+          <input 
+          type="text" 
+          placeholder="enter cryptocurrency"
+          className="coin-input" 
+          onChange={handleChange}/>
+        </form>
+      </div>
+      {filteredCoin.map(coin => {
+        return <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          marketcap={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+          />;
+      })}
     </div>
+
   );
 }
 
